8226c0b26ba5d589a118de8c959fb51b653d8093
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2025-03-01 21:40:26 +0700

    comit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const db = new sqlite3.Database(path.join(__dirname, '../database.sqlite'), (err) => {
+const db = new sqlite3.Database(path.join(__dirname, '../Database/database.sqlite'), (err) => {
     if (err) {
         console.error('Error connecting to SQLite:', err.message);
     } else {
